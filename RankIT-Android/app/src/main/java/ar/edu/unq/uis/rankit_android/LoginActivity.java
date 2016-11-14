bb73b6d58536a07affe9071796493a51c887efc7
nicolas <nicolastpi10@gmail.com>
2016-11-14 19:16:35 -0300

    modificado LoginActivity.java

--- RankIT-Android/app/src/main/java/ar/edu/unq/uis/rankit_android/LoginActivity.java
+++ RankIT-Android/app/src/main/java/ar/edu/unq/uis/rankit_android/LoginActivity.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.uis.rankit_android;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,43 +8,110 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText usuarioET;
-    private EditText passwordET;
-    private EditText errorMsgET;
-    private Button ingresarBtn;
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+    @Bind(R.id.input_email) EditText _emailText;
+    @Bind(R.id.input_password) EditText _passwordText;
+    @Bind(R.id.btn_login) Button _loginButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_rankit_login);
+        setContentView(R.layout.activity_rankit_login);
+        ButterKnife.bind(this);
 
-        this.errorMsgET = (EditText) this.findViewById(R.id.error_message);
-        this.errorMsgET.setVisibility(View.GONE);
-        this.usuarioET = (EditText) this.findViewById(R.id.usuario);
-        this.passwordET = (EditText) this.findViewById(R.id.password);
-        this.ingresarBtn = (Button) this.findViewById(R.id.ingresar);
+        _loginButton.setOnClickListener(new View.OnClickListener() {
 
-        this.ingresarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-
-                String usuario = usuarioET.getText().toString();
-                String contraseña = passwordET.getText().toString();
-
-                errorMsgET.setText(usuario+" "+contraseña);
-                errorMsgET.setVisibility(View.VISIBLE);
-
-                startCalificacionesListActivity();
+            public void onClick(View v) {
+                login();
             }
         });
+
+
     }
 
-    public void startCalificacionesListActivity(){
-        Intent detailIntent = new Intent(this, CalificacionesListActivity.class);
-        startActivity(detailIntent);
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
     }
+
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        //Start the new activity
+        Intent intent = new Intent(getApplicationContext(), CalificacionesListActivity.class);
+        startActivityForResult(intent, REQUEST_SIGNUP);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+
 }
