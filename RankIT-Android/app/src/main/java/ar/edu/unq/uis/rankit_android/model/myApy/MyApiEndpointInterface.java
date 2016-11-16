ea4f0674e85b580bdbd0f7b0e5f22ce60f919dc5
nicolas <nicolastpi10@gmail.com>
2016-11-16 01:35:05 -0300

    update My interface

--- RankIT-Android/app/src/main/java/ar/edu/unq/uis/rankit_android/model/myApy/MyApiEndpointInterface.java
+++ RankIT-Android/app/src/main/java/ar/edu/unq/uis/rankit_android/model/myApy/MyApiEndpointInterface.java
@@ -1,8 +1,15 @@
 package ar.edu.unq.uis.rankit_android.model.myApy;
 
 import ar.edu.unq.uis.rankit_android.model.clasesMinificadas.Minificadas;
-import retrofit.http.*;
-import retrofit.Call;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import java.util.List;
+
 
 /**
  * Created by nicolas on 16/11/16.
@@ -13,13 +20,13 @@ public interface MyApiEndpointInterface {
     @POST("usuarios")
     Call<Minificadas.LogUsuario> logInUsuario(@Body Minificadas.DatosUsuario datos);
     @GET("calificaciones")
-    Call<Minificadas.CalificacionMinificada> calificaciones(String id);
+    Call<List<Minificadas.CalificacionMinificada>> calificaciones(String idUsuario);
     @DELETE("calificaciones")
-    void delete(String id);
+    Call<ResponseBody> delete(String id);
     @PUT("calificaciones")
-    void editar(@Body Minificadas.CalificacionMinificada miniCalificacion);
+    Call<ResponseBody> editar(@Body Minificadas.CalificacionMinificada miniCalificacion);
     @POST("calificaciones")
-    void nuevaCalificacion(@Body Minificadas.CalificacionMinificada miniCalificacion);
+    Call<ResponseBody> nuevaCalificacion(@Body Minificadas.CalificacionMinificada miniCalificacion);
 
 
 }
